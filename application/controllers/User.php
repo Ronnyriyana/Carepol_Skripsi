71873d965e7324e765b336d51813542643761368
Ronnyriyana <riyanaronny@gmail.com>
2019-12-02 22:27:03 +0700

    Update controller user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Adminxuser extends CI_Controller {
+class User extends CI_Controller {
 	public function __construct(){    
 		parent::__construct();
 		//untuk template

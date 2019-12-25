736f72ae8cb127565a73537f3983e47aafd991f2
Ronnyriyana <riyanaronny@gmail.com>
2019-12-25 14:35:05 +0700

    update user

--- application/models/User_m.php
+++ application/models/User_m.php
@@ -4,7 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class User_m extends CI_Model {
 	public function Getuser()
 	{
-		$this->db->select("user.*, (SELECT COUNT(*) FROM alat WHERE alat.id_user=user.id_user) AS jumlah");
 		$data = $this->db->get("user");
 		return $data->result_array();
 	}
@@ -15,20 +14,26 @@ class User_m extends CI_Model {
 		return $data->result_array();
 	}
 	
-	function proses_update_profile($id_user,$data){
-		$this->db->where(array('id_user' => $id_user));
+	function proses_update($data){
+		$this->db->where(array('id_user' => $data['id_user']));
 		$res = $this->db->update('user',$data);
 		return $res;
 	}
 	
-	function proses_delete_data($id){
+	function proses_delete($id){
 		$this->db->where(array('id_user' => $id));
 		$res = $this->db->delete("user");
 		return $res;
 	}
 	
-	function proses_input_data($data){
+	function proses_input($data){
 		$res = $this->db->insert("user",$data);
 		return $res;
+	}
+
+	function photo($id){
+		$this->db->select("photo");
+		$data = $this->db->get_where("user", array('id_user' => $id));
+		return $data->row();
 	}
 }

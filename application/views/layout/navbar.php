ef47bbde511d1cc159bb25b80786d109c074fed8
Ronnyriyana <riyanaronny@gmail.com>
2019-12-26 11:44:41 +0700

    Upadate logo

--- application/views/layout/navbar.php
+++ application/views/layout/navbar.php
@@ -13,13 +13,13 @@
                     <ul class="nav navbar-nav navbar-right">
                         <li class="dropdown">
                               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <i class="ti-user"></i>
-									<p><?php echo $this->session->userdata('username');?></p>
-									<b class="caret"></b>
+                                    <i class="ti-user"> </i>
+                                    <p><?php echo $this->session->userdata('username');?></p>
+                                    <b class="caret"> </b>
                               </a>
                               <ul class="dropdown-menu">
                                 <!--<li><a href="#">Akun</a></li>-->
-                                <li><a href="<?php echo site_url('login/logout'); ?>">Logout</a></li>
+                                <li><a href="<?php echo site_url('login/logout'); ?>"><i class="ti-power-off"> </i>Logout</a></li>
                               </ul>
                         </li>
                     </ul>

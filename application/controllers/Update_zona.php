8e33a9757d0c3eecbf48222e89a3a90b0ec56a47
Ronnyriyana <riyanaronny@gmail.com>
2020-01-08 15:41:56 +0700

    link update zona

--- application/controllers/Update_zona.php
+++ application/controllers/Update_zona.php
@@ -27,16 +27,56 @@ class Update_zona extends CI_Controller {
 			$cek = $this->m->cari_parameter($zonasi['lat'],$zonasi['lon']);//cari parameter yg masuk zona & rata2kan
 			//if($cek==!null){
 				foreach($cek as $ceki){
-					$update = $this->m->updateZona($zonasi['id'],$ceki['gasr']);//update zona
+					$ceki['ispu'] = $this->ispu($ceki['co']);
+					$update = $this->m->updateZona($zonasi['id'],$ceki);//update zona
 				}
 			//}
 		}
 		if($update>=1){
-			echo "Berhasil";
+			$this->session->set_flashdata("berhasil","Zona berhasil di-update.");
+			redirect('map');
 		}
 		else{
-			echo "Gagal";
+			$this->session->set_flashdata("gagal","Zona gagal di-update.");
+			redirect('map');
 		}
+	}
+
+	private function ispu($co){
+		if($co <= 5){
+			$Ia=50;
+			$Ib=0;
+			$Xa=5;
+			$Xb=0;
+		}elseif($co <= 10){
+			$Ia=100;
+			$Ib=50;
+			$Xa=10;
+			$Xb=5;
+		}elseif($co <= 17){
+			$Ia=200;
+			$Ib=100;
+			$Xa=17;
+			$Xb=10;
+		}elseif($co <= 34){
+			$Ia=300;
+			$Ib=200;
+			$Xa=34;
+			$Xb=17;
+		}elseif($co <= 46){
+			$Ia=400;
+			$Ib=300;
+			$Xa=46;
+			$Xb=34;
+		}else{
+			$Ia=500;
+			$Ib=400;
+			$Xa=57.5;
+			$Xb=46;
+		}
+
+		$I = ((($Ia-$Ib)/($Xa-$Xb))*($co-$Xb))+$Ib;
+		return $I;
 	}
 
 }

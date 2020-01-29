2b9164d1c27cfe1dc48c2fed4b3ac00d5ba9b771
Ronnyriyana <riyanaronny@gmail.com>
2020-01-29 15:06:00 +0700

    ganti logo alat

--- application/views/layout/sidebar_admin.php
+++ application/views/layout/sidebar_admin.php
@@ -39,7 +39,7 @@
                 </li>
 				<li class="<?php echo isset($active_menu_alat)?$active_menu_alat:'' ?>">
                     <a href="<?php echo base_url('index.php/alat'); ?>">
-                        <i class="ti-tablet"></i>
+                        <i class="ti-signal"></i>
                         <p>Alat</p>
                     </a>
                 </li>

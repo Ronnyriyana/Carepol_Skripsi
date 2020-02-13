2061bc2cb9dd7e9ebc72e6f4f35df1092942e08d
Ronnyriyana <riyanaronny@gmail.com>
2020-02-13 15:22:10 +0700

    ada mqtt

--- application/views/dashboard/dashboard/dashboard.php
+++ application/views/dashboard/dashboard/dashboard.php
@@ -6,14 +6,14 @@
                   <div class="content">
                       <div class="row">
                           <div class="col-xs-5">
-                              <div class="icon-big icon-warning text-center">
-                                  <i class="ti-server"></i>
-                              </div>
+                            <div class="spinner-border text-success" role="status">
+                                <span class="sr-only">Loading...</span>
+                            </div>
                           </div>
                           <div class="col-xs-7">
                               <div class="numbers">
-                                  <p>Capacity</p>
-                                  105GB
+                                  <p>Zona</p>
+                                  <?= $zona; ?>
                               </div>
                           </div>
                       </div>
@@ -32,38 +32,13 @@
                       <div class="row">
                           <div class="col-xs-5">
                               <div class="icon-big icon-success text-center">
-                                  <i class="ti-wallet"></i>
+                                  <i class="ti-location-pin"></i>
                               </div>
                           </div>
                           <div class="col-xs-7">
                               <div class="numbers">
-                                  <p>Revenue</p>
-                                  $1,345
-                              </div>
-                          </div>
-                      </div>
-                      <div class="footer">
-                          <hr />
-                          <div class="stats">
-                              <i class="ti-calendar"></i> Last day
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          <div class="col-lg-3 col-sm-6">
-              <div class="card">
-                  <div class="content">
-                      <div class="row">
-                          <div class="col-xs-5">
-                              <div class="icon-big icon-danger text-center">
-                                  <i class="ti-pulse"></i>
-                              </div>
-                          </div>
-                          <div class="col-xs-7">
-                              <div class="numbers">
-                                  <p>Errors</p>
-                                  23
+                                  <p>Parameter</p>
+                                  <?= $parameter; ?>
                               </div>
                           </div>
                       </div>
@@ -81,14 +56,39 @@
                   <div class="content">
                       <div class="row">
                           <div class="col-xs-5">
-                              <div class="icon-big icon-info text-center">
-                                  <i class="ti-twitter-alt"></i>
+                              <div class="icon-big icon-danger text-center">
+                                  <i class="ti-user"></i>
                               </div>
                           </div>
                           <div class="col-xs-7">
                               <div class="numbers">
-                                  <p>Followers</p>
-                                  +45
+                                  <p>Pemilik Alat</p>
+                                  <?= $pemilik_alat; ?>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="footer">
+                          <hr />
+                          <div class="stats">
+                              <i class="ti-calendar"></i> Last day
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="col-lg-3 col-sm-6">
+              <div class="card">
+                  <div class="content">
+                      <div class="row">
+                          <div class="col-xs-5">
+                              <div class="icon-big icon-info text-center">
+                                  <i class="ti-signal"></i>
+                              </div>
+                          </div>
+                          <div class="col-xs-7">
+                              <div class="numbers">
+                                  <p>Alat</p>
+                                  <?= $alat; ?>
                               </div>
                           </div>
                       </div>
@@ -127,52 +127,23 @@
               </div>
           </div>
       </div>
-      <div class="row">
-          <div class="col-md-6">
-              <div class="card">
-                  <div class="header">
-                      <h4 class="title">Email Statistics</h4>
-                      <p class="category">Last Campaign Performance</p>
-                  </div>
-                  <div class="content">
-                      <div id="chartPreferences" class="ct-chart ct-perfect-fourth"></div>
-
-                      <div class="footer">
-                          <div class="chart-legend">
-                              <i class="fa fa-circle text-info"></i> Open
-                              <i class="fa fa-circle text-danger"></i> Bounce
-                              <i class="fa fa-circle text-warning"></i> Unsubscribe
-                          </div>
-                          <hr>
-                          <div class="stats">
-                              <i class="ti-timer"></i> Campaign sent 2 days ago
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          <div class="col-md-6">
-              <div class="card ">
-                  <div class="header">
-                      <h4 class="title">2015 Sales</h4>
-                      <p class="category">All products including Taxes</p>
-                  </div>
-                  <div class="content">
-                      <div id="chartActivity" class="ct-chart"></div>
-
-                      <div class="footer">
-                          <div class="chart-legend">
-                              <i class="fa fa-circle text-info"></i> Tesla Model S
-                              <i class="fa fa-circle text-warning"></i> BMW 5 Series
-                          </div>
-                          <hr>
-                          <div class="stats">
-                              <i class="ti-check"></i> Data information certified
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
+	  <div class="row">
+        <div class="biskuit_wafer">
+            <div id="messages"></div>
+        </div>
+	  </div>
   </div>
 </div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.2/mqttws31.min.js" type="text/javascript"></script>
+<link href="<?php echo base_url(); ?>assets/mqtt/style_dashboard.css" rel="stylesheet">
+<script src="<?php echo base_url(); ?>assets/mqtt/mqtt_setting.js"></script>
+<script>
+$(document).ready(function() {
+    startConnect();
+});
+function insertparameter(data){
+   $.post("<?= base_url('index.php/receiver_alat/store'); ?>", data, function(data, status){
+        document.getElementById("messages").innerHTML += '<span><b>Input: '+data+'</b></span><br/>';
+    });
+}
+</script>
